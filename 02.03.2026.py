4c4ca50360e58727a9401df37fdf06c754f970ad
Lobarxon <qodirovjavohir0199@gmail.com>
2026-03-02 16:00:50 +0300

    Create 02.03.2026.py

---
+++ 02.03.2026.py
@@ -0,0 +1,34 @@
+# from dataclasses import dataclass
+#
+#
+# @dataclass
+# class Mashina: #Main class (yoki ota class)
+#     rang: str
+#     model: str
+#     tezlik: float
+#
+#
+# @dataclass
+# class Avtobus(Mashina): # Meros
+#     o_soni: int
+
+
+# gentra = Mashina("Qora", "Gentra 1.8", 200)
+# #gentra.model = "Nexia"
+# isuzu = Avtobus("Oq", "Isuzu", 300, 50)
+
+# print(isuzu.o_soni)
+
+# class Mashina:
+#     def __init__(self, rang, model, tezlik)
+#         self.rang = rang #xususiyat
+#         self.__model = model
+#         self.__tezlik = tezlik
+
+#     def tormozla(self, qiymat):
+#         if self.__tezlik - qiymat
+
+
+# gentra = Mashina("Oq", "Jentra 1.6", 100)
+
+# print(gentra)
